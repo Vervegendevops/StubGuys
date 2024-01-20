ef2ebbd6046b1351ef494c814636c0b51f020573
Ankit <ankii8946@gmail.com>
2024-01-20 11:06:58 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:stub_guys/ATTANDEE_APP/A_Screens/HomeFeeds/HomeFeed.dart';
 import 'package:stub_guys/ORGANISER_APP/O_Screens/O_Profile/Components/O_ChnagePassword.dart';
 
 //hello
-// hello i am ankit
 void main() {
   runApp(const MyApp());
 }
