415556aa108fbe0e965cc85bea66bbdc210f28ab
yash <vervegendevops@gmail.com>
2024-01-20 13:04:08 +0530

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,6 @@ import 'package:stub_guys/ATTANDEE_APP/A_Screens/HomeFeeds/HomeFeed.dart';
 import 'package:stub_guys/ORGANISER_APP/O_Screens/O_Profile/Components/O_ChangePin.dart';
 import 'package:stub_guys/ORGANISER_APP/O_Screens/O_Profile/Components/O_ChnagePassword.dart';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-//hello to main
-
-=======
-//hello
->>>>>>> vgt
-=======
-
->>>>>>> vgt
 void main() {
   runApp(const MyApp());
 }
