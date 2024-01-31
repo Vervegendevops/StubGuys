4b017763951c8615652a98825e2306e97a53f281
Shrayansh <shrayansh@Shrayanshs-MacBook-Pro.local>
2024-01-31 17:02:46 +0530

    sdvZS

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      // home: HomeFeed(),
+      home: HomeFeed(),
       // home: ChatPage(),
-      home: PasswordPage(),
+      // home: PasswordPage(),
     );
   }
 }
